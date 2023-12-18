dda44be30bfb926c40004278b4b50fee725cd6c5
WillyMayer <will.mayer@outlook.com>
2023-12-18 19:53:05 -0300

    checkpoint

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
-
 const express = require('express');
+const blog = require('./routes/blog');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World')
-})
+app.get('/', blog)
 
 app.listen(process.env.PORT, console.log('Listening on port'))
